7357497e904efecdce57e48ed1057d4a747f1b91
YasarChavez <chavezyasar@gmail.com>
2023-11-17 21:30:07 -0300

    Init Proyecto

---
+++ src/main/java/com/vanguardiapropiedades/inmobiliaria/InmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.vanguardiapropiedades.inmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmobiliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanguardiapropiedades/inmobiliaria/InmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanguardiapropiedades.inmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
