6fe6577eff284825c2239c19c2e630839f7d88d3
AugusL <augustolaguna30@yahoo.com>
2023-11-20 21:01:46 -0300

    Entidad propiedad y Roles de propiedad

--- src/main/java/com/vanguardiapropiedades/inmobiliaria/Entities/Property.java
+++ src/main/java/com/vanguardiapropiedades/inmobiliaria/Entities/Property.java
@@ -5,31 +5,28 @@ import java.util.ArrayList;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.vanguardiapropiedades.inmobiliaria.Enums.Rol;
+import com.vanguardiapropiedades.inmobiliaria.Enums.Tipo;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Entity
 @Data
-public class User {
-
+public class Property {
+    
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
-
-    private String nombre;
-    private String email;
-    private String password;
-    private ArrayList<Property> propiedades;
+    private int precio;
     @Enumerated(EnumType.STRING)
-    private Rol rol;
-    @OneToOne
-    private MultipartFile imagen;
+    private Tipo tipo;
+    private User usuario;
+    private ArrayList<MultipartFile> imagen;
+    private Boolean estado;
+    
 }
