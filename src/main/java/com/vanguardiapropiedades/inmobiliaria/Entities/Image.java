94233c9eb3ffe50a5ec35ff883febd28cd324d9c
YasarChavez <chavezyasar@gmail.com>
2023-11-20 23:38:19 -0300

    Arreglando las Relaciones

---
+++ src/main/java/com/vanguardiapropiedades/inmobiliaria/Entities/Image.java
@@ -0,0 +1,33 @@
+package com.vanguardiapropiedades.inmobiliaria.Entities;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Image {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2") // Estrategia alternativa
+    private String id;
+    private String mime;
+    private String nombre;
+    @ManyToOne
+    @JoinColumn(name = "property_id", nullable = false)
+    private Property propiedad;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] contenido;
+}
--- src/main/java/com/vanguardiapropiedades/inmobiliaria/Entities/User.java
+++ src/main/java/com/vanguardiapropiedades/inmobiliaria/Entities/User.java
@@ -1,9 +1,8 @@
 package com.vanguardiapropiedades.inmobiliaria.Entities;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.GenericGenerator;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.vanguardiapropiedades.inmobiliaria.Enums.Rol;
 
@@ -12,7 +11,10 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+
 import lombok.Data;
 
 @Entity
@@ -23,13 +25,13 @@ public class User {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
-
     private String nombre;
     private String email;
     private String password;
-    private ArrayList<Property> propiedades;
     @Enumerated(EnumType.STRING)
     private Rol rol;
+    @OneToMany
+    private List<Property> propiedades;
     @OneToOne
-    private MultipartFile imagen;
+    private Image imagen;
 }
