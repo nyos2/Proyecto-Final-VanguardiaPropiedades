95a6d087371e226fad0275bbf96696733f34be56
edengole31 <egonzalez.tecnico@gmail.com>
2023-11-23 21:03:39 -0300

    Delete y Editar

--- src/main/java/com/vanguardiapropiedades/inmobiliaria/Services/PropertyService.java
+++ src/main/java/com/vanguardiapropiedades/inmobiliaria/Services/PropertyService.java
@@ -37,9 +37,33 @@ public class PropertyService {
             property.setEstado(true);
             propertyRepository.save(property);
         }
+
     }
 
-    public void validar(Integer precio, String tipo) throws MyException{
+    @Transactional
+    public void editarProperty(String id, int precio, String tipo, String idUser) throws MyException {
+        validar(precio,tipo);
+
+        Optional<UserEntity> respuesta = userRepository.findById(idUser);
+        Optional<Property> respuestaP= propertyRepository.findById(id);
+         
+
+        if (respuesta.isPresent()) {
+
+            UserEntity usuario = respuesta.get();
+        }
+        if (respuestaP.isPresent()){
+            Property property= respuestaP.get();
+            property.setPrecio(precio);
+            property.setTipo(Tipo.valueOf(tipo));
+            property.setUsuario(usuario);
+            property.setEstado(true);
+            propertyRepository.save(property);
+        }
+
+    }
+
+    public void validar(Integer precio, String tipo) throws MyException {
 
         if (precio == null) {
             throw new MyException("El precio no puede estar vacio");
@@ -50,17 +74,22 @@ public class PropertyService {
         }
 
     }
+
     // Listar propiedades
-    public List<Property> listarProperty(){
-        
+    public List<Property> listarProperty() {
+
         List<Property> propertys = propertyRepository.findAll();
         return propertys;
     }
-    //Buscar por tipo de propiedad
+
+    // Buscar por tipo de propiedad
     public Optional<Property> buscarPorTipo(String tipo) {
         Property property = propertyRepository.findByTipo(tipo);
         return Optional.ofNullable(property);
     }
-
+     //eliminar propiedad
+    public void deleteUser(String Id) {
+        userRepository.deleteById(Id);
+    }
 
 }
