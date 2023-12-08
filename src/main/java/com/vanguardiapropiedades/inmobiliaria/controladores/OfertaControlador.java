024d2e0bdb8a5709c2f8efae955e5a71985d1295
Edu Moreno <e.m.morenolp@gmail.com>
2023-12-08 01:39:59 -0300

    Barra de busqueda y HTML Ofertas

--- src/main/java/com/vanguardiapropiedades/inmobiliaria/controladores/OfertaControlador.java
+++ src/main/java/com/vanguardiapropiedades/inmobiliaria/controladores/OfertaControlador.java
@@ -1,13 +1,19 @@
 package com.vanguardiapropiedades.inmobiliaria.controladores;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.vanguardiapropiedades.inmobiliaria.entidades.OfertaEntidad;
 import com.vanguardiapropiedades.inmobiliaria.excepciones.MiException;
 import com.vanguardiapropiedades.inmobiliaria.servicios.OfertaServicio;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 @RequestMapping("/oferta")
@@ -15,6 +21,14 @@ public class OfertaControlador {
 
     @Autowired
     private OfertaServicio ofertaServicio;
+
+    @GetMapping("/ofertas")
+    public String listaOfertas(ModelMap modelo) {
+        // Obtener la lista de ofertas y agregarla al modelo
+        List<OfertaEntidad> ofertas = ofertaServicio.obtenerTodasLasOfertas();
+        modelo.put("ofertas", ofertas);
+        return "Propiedades/propiedad_ofertas.html";
+    }
 
     @PostMapping("/crear")
     public String crearOferta(Integer valorOferta, String propiedadId, String usuarioId,
--- src/main/java/com/vanguardiapropiedades/inmobiliaria/servicios/OfertaServicio.java
+++ src/main/java/com/vanguardiapropiedades/inmobiliaria/servicios/OfertaServicio.java
@@ -1,6 +1,7 @@
 package com.vanguardiapropiedades.inmobiliaria.servicios;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,6 +57,11 @@ public class OfertaServicio {
     @Transactional
     public void eliminarOferta(String id) throws MiException {
         ofertaRepositorio.deleteById(id);
+    }
+
+    @Transactional
+    public List<OfertaEntidad> obtenerTodasLasOfertas() {
+        return ofertaRepositorio.findAll();
     }
 
 }
